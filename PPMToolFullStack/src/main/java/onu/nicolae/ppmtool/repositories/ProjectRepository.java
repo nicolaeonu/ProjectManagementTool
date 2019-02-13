ba0da5880117c3daaa28efe7b330bf06b91d6b77
Nicolae <nicolae.onu@outlook.com>
2019-02-13 10:39:28 +0200

    Project Obj & Project Repository

---
+++ PPMToolFullStack/src/main/java/onu/nicolae/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package onu.nicolae.ppmtool.repositories;
+
+import onu.nicolae.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
